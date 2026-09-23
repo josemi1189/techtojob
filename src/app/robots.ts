825b25d847a8ce6a6e9dd0dc29ee4740f59c1559
josemi1189 <jose1189@gmail.com>
2026-09-23 17:41:29 +0200

    fix: update SITE_URL to use window.location.origin for correct sitemap generation

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const SITE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const SITE_URL = window.location.origin;
 
 export default function robots(): MetadataRoute.Robots {
   return {
